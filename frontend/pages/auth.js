f4ee3e1cbc16f3ac675eabe1d3157607502b8bb2
morozovxc <98089803+morozovxc@users.noreply.github.com>
2022-07-15 16:15:54 +0500

    Added Main

--- frontend/pages/auth.js
+++ frontend/pages/auth.js
@@ -6,6 +6,7 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Checkbox from '@mui/material/Checkbox';
 import { FormControlLabel } from "@mui/material";
+import Head from "next/head";
 
 export default function Auth() {
     const [hide, setHide] = useState(true);
@@ -37,25 +38,31 @@ export default function Auth() {
     }
 
     return (
-        <Container>
-            <Grid
-                container
-                spacing={0}
-                direction="column"
-                alignItems="center"
-                justifyContent="center"
-                style={{ minHeight: '100vh' }}
-            >
-                <form onSubmit={submitLogin}>
-                    <Stack spacing={2} justifyContent={'center'} sx={{width: "400px"}}>
-                        <TextField error={!!error.login} value={form.login} name='login' label="Логин" helperText={error.login} variant="outlined" type='text' className='login' onChange={handleFormChange} />
-                        <TextField error={!!error.password} value={form.password} name='password' label="Пароль" helperText={error.password} variant="outlined" type={hide ? 'password' : 'text'} className='password' onChange={handleFormChange} />
-                        <FormControlLabel control={<Checkbox checked={!hide} onClick={hidePassword} size='small' />} label="Показать пароль" />
-                        <Button variant="contained" type={'submit'}>Далее</Button>
-                    </Stack>
-                </form>
-            </Grid>
-        </Container>
+        <div>
+            <Head>
+                <title>Студентто - Профиль</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Container>
+                <Grid
+                    container
+                    spacing={0}
+                    direction="column"
+                    alignItems="center"
+                    justifyContent="center"
+                    style={{ minHeight: '100vh' }}
+                >
+                    <form onSubmit={submitLogin}>
+                        <Stack spacing={2} justifyContent={'center'} sx={{width: "400px"}}>
+                            <TextField error={!!error.login} value={form.login} name='login' label="Логин" helperText={error.login} variant="outlined" type='text' className='login' onChange={handleFormChange} />
+                            <TextField error={!!error.password} value={form.password} name='password' label="Пароль" helperText={error.password} variant="outlined" type={hide ? 'password' : 'text'} className='password' onChange={handleFormChange} />
+                            <FormControlLabel control={<Checkbox checked={!hide} onClick={hidePassword} size='small' />} label="Показать пароль" />
+                            <Button variant="contained" type={'submit'}>Далее</Button>
+                        </Stack>
+                    </form>
+                </Grid>
+            </Container>
+        </div>
     )
 }
 
