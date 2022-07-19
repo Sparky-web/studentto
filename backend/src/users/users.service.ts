76bff6d4d27f476b2f303b8d777007be364b96ec
Vladislav <babinovvlad@gmail.com>
2022-07-19 22:35:50 +0500

    upd

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -8,7 +8,6 @@ export type User = any;
 
 @Injectable()
 export class UsersService {
-<<<<<<< HEAD
     constructor(private readonly strapi: StrapiService) { }
 
     async getOne(username: string): Promise<AppUser | undefined> {
@@ -17,18 +16,5 @@ export class UsersService {
 
     async getAll(): Promise<Array<AppUser>> {
         return await this.strapi.get("app-users")
-=======
-    async findOne(username: string): Promise<User | undefined> {
-        try {
-            const { data: { data: [{ attributes: user }] } } = await axios.get("http://localhost:1337/api/app-users?" + stringify({
-                filters: {
-                    username
-                }
-            }))
-            return user
-        } catch (e) {
-            return null
-        }
->>>>>>> 727bbd65ce5eb07032dfb22d0a0ce52961cde8bf
     }
 }
