1d7b36bc3bbd11501e4280b6b2b8b05bf6f75675
Vladislav <babinovvlad@gmail.com>
2022-07-22 15:39:18 +0500

    upd

--- frontend/pages/auth.js
+++ frontend/pages/auth.js
@@ -1,81 +1,26 @@
-import { useReducer, useState } from "react";
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
-import Checkbox from '@mui/material/Checkbox';
-import { FormControlLabel } from "@mui/material";
 import Head from "next/head";
 import ButtonBar from "../components/ButtonBar";
-import strapi from "../modules/strapi";
-import { useRouter } from "next/router";
+import AuthForm from "../components/AuthForm"
 
 export default function Auth() {
-    const [hide, setHide] = useState(true);
-    const [error, setError] = useState({ login: '', password: '' })
-    const router = useRouter()
-
-    const intValues = {
-        login: '',
-        password: ''
-    }
-
-    const [form, setForm] = useReducer(
-        (curValues, newValues) => ({ ...curValues, ...newValues }), intValues
-    )
-
-    function handleFormChange(event) {
-        const { name, value } = event.target;
-        setForm({ [name]: value });
-    }
-
-    function hidePassword() { setHide(!hide); }
-
-    async function submitLogin(e) {
-        try {
-            e.preventDefault();
-            const error = {
-                login: !form.login && "Строка не должна быть пустой",
-                password: !form.password && "Строка не должна быть пустой"
-            }
-            setError(error);
-
-            await strapi.auth(form.login, form.password)
-            router.push("/profile")
-        } catch (e) {
-            console.error(e)
-            alert("Ошибка")
-        }
-    }
-
-
     return (
         <div>
             <Head>
                 <title>Студентто - Авторизация</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Container>
-                <Grid
-                    container
-                    spacing={0}
-                    direction="column"
-                    alignItems="center"
-                    justifyContent="center"
-                    style={{ minHeight: '100vh' }}
-                >
-                    <form onSubmit={submitLogin}>
-                        <Stack spacing={2} justifyContent={'center'} sx={{ width: "400px" }}>
-                            <TextField error={!!error.login} value={form.login} name='login' label="Логин" helperText={error.login} variant="outlined" type='text' className='login' onChange={handleFormChange} />
-                            <TextField error={!!error.password} value={form.password} name='password' label="Пароль" helperText={error.password} variant="outlined" type={hide ? 'password' : 'text'} className='password' onChange={handleFormChange} />
-                            <FormControlLabel control={<Checkbox checked={!hide} onClick={hidePassword} size='small' />} label="Показать пароль" />
-
-                            <Button variant="contained" type={'submit'}>Далее</Button>
-                        </Stack>
-                    </form>
-                </Grid>
-            </Container>
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                style={{ minHeight: '100vh' }}
+            >
+                <AuthForm />
+            </Grid>
             <ButtonBar value={1} />
         </div>
     )
---
+++ frontend/pages/students/index.js
@@ -0,0 +1,148 @@
+import Head from 'next/head'
+import { useState } from "react";
+import { Modal, Typography } from "@mui/material";
+import Stack from "@mui/material/Stack";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Lesson from "../../components/Lesson.js"
+import ButtonBar from "../../components/ButtonBar"
+import {DateTime} from "luxon";
+
+const modalStyles = {
+    position: 'absolute', backgroundColor: '#FFFFF9', boxShadow: 24,  borderRadius: '10px',
+    width: '90vw', maxWidth: "500px", height: '500px', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'
+}
+
+export default function Home() {
+    const [modalData, setModalData] = useState(null);
+
+    const time = DateTime.now().setZone("system").setLocale("ru");
+    const [data, setData] = useState({
+        date: '20.11.2022',
+        week: time.toFormat("EEEE"),
+        lessons: [
+            {
+                id: 0,
+                start: '8:30',
+                end: '10:00',
+                name: 'Математика',
+                professor: {
+                    firstName: 'Алексей',
+                    lastName: 'Еремеев',
+                    veryLastName: 'Леонидович'
+                },
+                comment: ""
+            }, {
+                id: 1,
+                start: '10:10',
+                end: '11:40',
+                name: 'Русский',
+                professor: {
+                    firstName: 'Наталья',
+                    lastName: 'Соколова',
+                    veryLastName: 'Георгиевная'
+                },
+                comment: ""
+            }, {
+                id: 2,
+                start: '12:10',
+                end: '13:40',
+                name: 'Литература',
+                professor: {
+                    firstName: 'Наталья',
+                    lastName: 'Соколова',
+                    veryLastName: 'Георгиевная'
+                },
+                comment: ""
+            }, {
+                id: 3,
+                start: '14:10',
+                end: '15:40',
+                name: 'Информатика',
+                professor: {
+                    firstName: 'Андрей',
+                    lastName: 'Зырянов',
+                    veryLastName: 'Владимирович'
+                },
+                comment: ""
+            }, {
+                id: 4,
+                start: '16:00',
+                end: '17:30',
+                name: 'Информатика',
+                professor: {
+                    firstName: 'Андрей',
+                    lastName: 'Зырянов',
+                    veryLastName: 'Владимирович'
+                },
+                comment: ""
+            }, {
+                id: 5,
+                start: '18:00',
+                end: '19:30',
+                name: 'Физика',
+                professor: {
+                    firstName: 'Александр',
+                    lastName: 'Кравцов',
+                    veryLastName: 'Сергеевич'
+                },
+                comment: ""
+            }, {
+                id: 6,
+                start: '19:40',
+                end: '21:10',
+                name: 'Физика',
+                professor: {
+                    firstName: 'Александр',
+                    lastName: 'Кравцов',
+                    veryLastName: 'Сергеевич'
+                },
+                comment: ""
+            }
+        ]
+    });
+
+    return (
+        <div>
+            <Head>
+                <title>Студентто</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <Grid
+                container
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                paddingBottom={8}
+            >
+                <Typography>{data.date}</Typography>
+                <Box sx={{ width: '380px', height: '3px', backgroundColor: '#000000' }} />
+
+                <Stack sx={{ width: '100%', maxWidth: 380 }} spacing={1}>
+                    {data.lessons.map(lesson => <Lesson lesson={lesson} setModalData={setModalData} key={lesson.id} />)}
+                </Stack>
+            </Grid>
+
+            <Modal
+                open={!!modalData}
+                onClose={() => setModalData(null)}
+            >
+                <Box sx={modalStyles}>
+                    <Grid
+                        container
+                        direction="column"
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <Typography style={ {color: '#00bfff', paddingTop: 10} }>{modalData?.name}</Typography>
+                        <Typography style={ {color: '#00bfff'} }>{data.date} ({data.week})</Typography>
+                        <Typography style={ {color: '#00bfff'} }>{modalData?.professor.lastName} {modalData?.professor.firstName} {modalData?.professor.veryLastName}</Typography>
+                    </Grid>
+                </Box>
+            </Modal>
+            
+            <ButtonBar value={0}/>
+        </div>
+    )
+}
