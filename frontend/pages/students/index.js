9bf61fab95e73f4170ffc36704b5ef4d56e57737
Vladislav <babinovvlad@gmail.com>
2022-07-24 21:06:48 +0500

    upd

--- frontend/pages/students/index.js
+++ frontend/pages/students/index.js
@@ -1,33 +1,57 @@
 import Head from 'next/head'
-import { useState } from "react";
-import { Modal, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { LinearProgress, Modal, Typography } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Lesson from "../../components/Lesson.js"
 import ButtonBar from "../../components/ButtonBar"
-import {DateTime} from "luxon";
+import { DateTime } from "luxon";
+import strapi from "../../modules/strapi"
 
 export default function Home() {
     const time = DateTime.now().setZone("system").setLocale("ru");
-    const [data, setData] = useState({
-        lessons: [
-            {
-                id: 0,
-                start: '8:30',
-                end: '10:00',
-                name: 'Математика',
-                professor: {
-                    firstName: 'Алексей',
-                    lastName: 'Еремеев',
-                    veryLastName: 'Леонидович'
-                },
-                date: '20.11.2022',
-                week: time.toFormat("EEEE"),
-                comment: ""
-            }
-        ]
-    });
+    // const [data, setData] = useState({
+    //     lessons: [
+    //         {
+    //             id: 0,
+    //             start: '8:30',
+    //             end: '10:00',
+    //             name: 'Математика',
+    //             professor: {
+    //                 firstName: 'Алексей',
+    //                 lastName: 'Еремеев',
+    //                 veryLastName: 'Леонидович'
+    //             },
+    //             date: '20.11.2022',
+    //             week: time.toFormat("EEEE"),
+    //             comment: ""
+    //         }
+    //     ]
+    // });
+
+    const [lessons, setLessons] = useState(null)
+
+    useEffect(() => {
+        async function init() {
+            const data = await strapi.get("lessons", {
+                filters: {
+                    group: 1,
+                    from: {
+                        // начало недели
+                        $gt: (new Date()).toISOString(),
+                        // конец недели
+                        $lt: (new Date()).toISOString()
+                    }
+                }
+            })
+            setLessons(data)
+            console.log(data)
+        }
+
+        init()
+    }, [])
+
 
     return (
         <div>
@@ -47,11 +71,12 @@ export default function Home() {
                 <Box sx={{ width: '380px', height: '3px', backgroundColor: '#000000' }} />
 
                 <Stack sx={{ width: '100%', maxWidth: 380 }} spacing={1}>
-                    {data.lessons.map(lesson => <Lesson lesson={lesson} key={lesson.id} />)}
+                    {lessons && lessons.map(lesson => <Lesson lesson={lesson} key={lesson.id} />)}
+                    {!lessons && <LinearProgress />}
                 </Stack>
             </Grid>
 
-            <ButtonBar value={0}/>
+            <ButtonBar value={0} />
         </div>
     )
 }
