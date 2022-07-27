3eb965af7cd1bf1a42df5fff1ff6c99fe18d8312
morozovxc <98089803+morozovxc@users.noreply.github.com>
2022-07-27 15:58:29 +0500

    upd

--- frontend/components/WeekBar.js
+++ frontend/components/WeekBar.js
@@ -3,13 +3,16 @@ import { useState } from "react";
 import WeekTab from "./WeekTab"
 
 export default function WeekBar( props ){
-    const [target, setTarget] = useState("0")
+    const [target, setTarget] = useState(0)
     return (
-        <Tabs
-            onChange={(event, newValue) => setTarget(newValue)}
-            value={target}
-        >
-            <WeekTab target={target} id={0} day={"Пн"} number={15}/>
+        <Tabs value={target}>
+            <WeekTab target={target} setTarget={setTarget} id={0} day={"Пн"} number={15}/>
+            <WeekTab target={target} setTarget={setTarget} id={1} day={"Вт"} number={16}/>
+            <WeekTab target={target} setTarget={setTarget} id={2} day={"Ср"} number={17}/>
+            <WeekTab target={target} setTarget={setTarget} id={3} day={"Чт"} number={18}/>
+            <WeekTab target={target} setTarget={setTarget} id={4} day={"Пт"} number={19}/>
+            <WeekTab target={target} setTarget={setTarget} id={5} day={"Сб"} number={20}/>
+            <WeekTab target={target} setTarget={setTarget} id={6} day={"Вс"} number={21}/>
         </Tabs>
     )
 }
--- frontend/components/WeekTab.js
+++ frontend/components/WeekTab.js
@@ -1,10 +1,11 @@
 import {Tab, Typography} from "@mui/material";
 
 export default function WeekTab( props ){
-    const { id, target, day, number } = props
+    const { id, target, day, number, setTarget } = props
     return (
         <Tab
-            value="0"
+            value={id}
+            onClick={(event) => setTarget(id)}
             icon={ <Typography color={target === id ? "primary.highlight" : "primary.neutral"} variant="captionM">{day}</Typography> }
             label={ <Typography color={target === id ? "primary.lightest" : "text.disabled"} variant="bodyL">{number}</Typography>}
             sx={ {minWidth: 0, borderRadius: "10px", backgroundColor: target === id ? "primary.main" : "primary.none" } }
