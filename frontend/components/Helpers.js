5f4ea09e706411d02efd319e7ebe974087ad644c
morozovxc <98089803+morozovxc@users.noreply.github.com>
2022-07-30 15:06:04 +0500

    fixx

--- frontend/components/Helpers.js
+++ frontend/components/Helpers.js
@@ -1,39 +1,33 @@
 import { DateTime } from "luxon";
-
+const getDate = ( time ) => { return ( DateTime.fromISO( time ).setLocale('ru') ) }
 export default function transformLesson ( lessons ) {
-
     const days = []
-
-    function addDay ( lesson , time ) {
+    const addDay = ( lesson , time ) => {
         const day = {
             dayIndex: time.toFormat('d'),
             weekday: time.toFormat('cccc'),
             shortWeekDay: time.toFormat('ccc'),
-            fullDayTitle: time.toFormat('cccc D'),//toLocaleString(DateTime.DATE_HUGE)
+            fullDayTitle: time.toFormat('cccc D'),
             lessons: [
                 lesson
             ]
         }
         days.push(day)
     }
-
     lessons?.map(lesson => {
-            const time = DateTime.fromISO(lesson.from).setLocale('ru')
             if (!days.length) {
-                addDay( lesson , time )
+                addDay( lesson , getDate(lesson.from) )
             } else {
                 const lineMassive = days.length
                 for(let i = 0; i < lineMassive; i++){
-                    //console.log(lesson.from === days[i].lessons[0].from)
-                    if(DateTime.fromISO(lesson.from).toFormat('D') === DateTime.fromISO(days[i].lessons[0].from).toFormat('D')){
+                    if(getDate( lesson.from ).toFormat('D') === getDate( days[i].lessons[0].from ).toFormat('D')){
                         days[i].lessons.push(lesson)
                         return;
                     }
                 }
-                addDay( lesson , time )
+                addDay( lesson , getDate(lesson.from) )
             }
         }
     )
-
     console.log(days)
 }
